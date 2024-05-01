bc626fce67b417db7686210ff3a610b0dd8eec73
Shazzadul-Shakib <shakib1186@gmail.com>
2024-05-01 15:08:33 +0600

    router setup and layout created

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+
+const Sidebar = () => {
+    return (
+        <div className=" bg-tertiary">
+            <h1 className=" text-accent">Sidebar</h1>
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar/Sidebar";
+
+const Main = () => {
+    return (
+        <div className=" bg-primary min-h-screen">
+            <Sidebar/>
+        </div>
+    );
+};
+
+export default Main;
